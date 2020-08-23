7face1afbf0cb60c25644a23175587d3fc954af6
Ronnie <teenbhaiacc@gmail.com>
2020-08-24 00:01:32 +0100

    added the basket service and entities to work with

---
+++ src/Basket/Basket.Api/Entities/BasketCartItem.cs
@@ -0,0 +1,17 @@
+﻿namespace Basket.Api.Entities
+{
+    public class BasketCartItem
+    {
+        public int Qunatity { get; set; }
+
+        public string Color { get; set; }
+
+        public decimal Price { get; set; }
+
+
+        public string ProductId { get; set; }
+
+        public string ProductName{ get; set; }
+
+    }
+}
